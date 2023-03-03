211b82d375ada8561f1dd095ce2e52931cf751bb
Tina Yang <Tinayangers@gmail.com>
2023-03-03 15:15:12 -0500

    Added user_task seeds

--- seeders/20230303164501-tasks.js
+++ seeders/20230303164501-tasks.js
@@ -1,7 +1,6 @@
 'use strict';
 const { User, Category } = require('../models')
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('tasks', [
