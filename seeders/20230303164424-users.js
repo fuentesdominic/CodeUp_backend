a9399efb2eafb6eca5424f7300c7ec2fb16aee4b
Tina Yang <Tinayangers@gmail.com>
2023-03-03 12:06:53 -0500

    Added users seeders

--- seeders/20230303164424-users.js
+++ seeders/20230303164424-users.js
@@ -2,24 +2,23 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          email: 'john@gmail.com',
+          password: '1234',
+          name: 'John Doe',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
