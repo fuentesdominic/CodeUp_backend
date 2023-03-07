e7ffd633701bb2a5eadd28880b3a6b687fc0f693
Tina Yang <Tinayangers@gmail.com>
2023-03-06 21:17:12 -0500

    deleted html/css/react/js routes and controllers

--- routes/UserTaskRouter.js
+++ routes/UserTaskRouter.js
@@ -3,13 +3,7 @@ const controller = require("../controllers/UserTaskController");
 const middleware = require("../middleware");
 
 router.get("/", controller.GetUserTasks);
-
 router.get('/:id', controller.GetUserTaskByTaskId)
-
-router.get('/html', controller.GetHtmlNotes)
-router.get('/:id/css', controller.GetCssNotes)
-router.get('/:id/js', controller.GetJavaScriptNotes)
-router.get('/:id/react', controller.GetReactNotes)
 router.post(
   "/",
   middleware.stripToken,
